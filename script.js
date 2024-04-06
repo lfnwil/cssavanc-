e3587db3b162c14ed212056d7f1fee1e41bd11b4
william <lfnwilliam@gmail.com>
2024-04-06 14:58:52 +0200

    Initial commit: ajout des fichiers du site web

---
+++ script.js
@@ -0,0 +1,9 @@
+const dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var experienceLink = document.getElementById('experience-link');
+});
